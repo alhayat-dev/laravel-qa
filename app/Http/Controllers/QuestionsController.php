0ba10573ffb13176eb0a26da1d31593973dc8fbe
alhayat-dev <alhayat.dev@gmail.com>
2020-03-07 10:36:08 -0500

    Implement the form validation and save the question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AskQuestionRequest;
 use App\Question;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -36,12 +37,13 @@ class QuestionsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param AskQuestionRequest $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->all());
+        return redirect()->route('questions.index')->with('success', 'Your question has been submitted successfully.');
     }
 
     /**

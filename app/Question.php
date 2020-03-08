03c178184360379686d4aa697eb618e4429d3865
alhayat-dev <alhayat.dev@gmail.com>
2020-03-07 22:52:54 -0500

    Design answers table schema and renamed the answers column in questions table as answers_count

--- app/Question.php
+++ app/Question.php
@@ -48,12 +48,16 @@ class Question extends Model
      * @return string
      */
     public function getStatusAttribute(){
-        if ($this->answers > 0){
+        if ($this->answers_count > 0){
             if ($this->best_answer_id){
                 return "answered-accepted";
             }
             return "answered";
         }
         return "unanswered";
+    }
+
+    public function answers(){
+        return $this->hasMany(Answer::class);
     }
 }

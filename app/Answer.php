4009089381593ca38c9a566c1e693526f9fa449c
alhayat-dev <alhayat.dev@gmail.com>
2020-03-08 21:34:57 -0400

    Generate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use function foo\func;
 
 class Answer extends Model
 {
@@ -14,5 +15,14 @@ class Answer extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static ::created(function ($answer){
+            $answer->question->increment('answers_count');
+        });
+
     }
 }
